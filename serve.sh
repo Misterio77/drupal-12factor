8e0a91e99670b58c3a0bff696327add07db829cd
Gabriel Fontes <hi@m7.rs>
2023-03-08 19:05:59 -0300

    improvements to serve script

--- serve.sh
+++ serve.sh
@@ -1,33 +1,46 @@
 #!/usr/bin/env bash
 
+set -e
+
 main_port="${1:-"8080"}"
 files_port="${2:-"8081"}"
 
-if ! command -v webfsd &> /dev/null; then
-  echo "You need webfsd to run this. Try 'nix develop'."
-  exit 1
-fi
+function require_cmd() {
+  if ! which $1 &> /dev/null; then
+    echo "You need $1 to run this script."
+  fi
+}
 
-if ! command -v php &> /dev/null; then
-  echo "You need php to run this. Try 'nix develop'."
-  exit 1
-fi
+require_cmd "webfsd"
+require_cmd "php"
 
-export DRUPAL_PROJECT_PATH="${DRUPAL_PROJECT_PATH:-"$(dirname "$(realpath -s "$0")")"}"
-export DRUPAL_DATA_PATH="${DRUPAL_DATA_PATH:-"$DRUPAL_PROJECT_PATH/data"}"
+here="$(dirname "$(realpath -s "$0")")"
+DRUPAL_PROJECT_PATH="${DRUPAL_PROJECT_PATH:-$here}"
+DRUPAL_DATA_PATH="${DRUPAL_DATA_PATH:-"$DRUPAL_PROJECT_PATH/data"}"
+
+pushd "$DRUPAL_PROJECT_PATH"
+
+if [ ! -d "$DRUPAL_PROJECT_PATH/vendor" ]; then
+  echo "Vendor dir $DRUPAL_PROJECT_PATH/vendor does not exist."
+  echo "Running 'composer install'..."
+  composer install
+fi
 
 if [ ! -d "$DRUPAL_DATA_PATH" ]; then
   echo "Data dir $DRUPAL_DATA_PATH does not exist."
-  echo "Running 'drush site:install' for you."
-  composer exec drush --root "$DRUPAL_PROJECT_PATH" si -vvv
+  echo "Running 'drush si'..."
+  composer exec drush si
 fi
 
-webfsd -F -p $files_port -r "$DRUPAL_DATA_PATH/public" &
 
-export DRUPAL_FILE_PUBLIC_BASE_URL="http://localhost:$files_port"
+webfsd -F -p $files_port -r "$DRUPAL_DATA_PATH/public" &
+DRUPAL_FILE_PUBLIC_BASE_URL="http://localhost:$files_port"
 echo "File server running ($DRUPAL_FILE_PUBLIC_BASE_URL)"
 
+export DRUPAL_PROJECT_PATH DRUPAL_DATA_PATH DRUPAL_FILE_PUBLIC_BASE_URL;
 php -S "localhost:$main_port" -t "$DRUPAL_PROJECT_PATH/web/" &
 
+
+popd
 trap 'kill $(jobs -p -r)' SIGINT
 wait
