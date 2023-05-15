b21e4d13696f531c53f66e404b98502e1e8b1f2b
Gabriel Fontes <hi@m7.rs>
2023-05-15 18:24:05 -0300

    improve 12f script a performance a little

--- web/12fdrupal.settings.php
+++ web/12fdrupal.settings.php
@@ -8,14 +8,11 @@
 # Everything has sane defaults, and is configurable through environment variables.
 # I did as much as I could to avoid mixing source code (such as this) with secrets and data.
 
-use Drupal\Component\Utility\Crypt;
-
 # Mutable data path
 # Default to a "data" directory in the project root path
 $root_data = getenv('DRUPAL_DATA_PATH') ?: realpath('../') . '/data';
 # Site-specific data dir
 $data = getenv("DRUPAL_{$site}_DATA_PATH") ?: "$root_data/sites/$site";
-@mkdir($data, recursive: true);
 
 # Use sqlite by default, it's a lot more convenient for local development
 # For production, using mysql or postgres is preferrable.
@@ -29,34 +26,43 @@ $databases['default']['default'] = array(
   'port' =>     getenv("DRUPAL_{$site}_DB_PORT")     ?: '',
 );
 
+# Location to keep the actual public files on
+# If we change this away from the webroot, Drupal will not serve them
+# So we'll keep the default and symlink this to it.
+$file_public_path_real = getenv("DRUPAL_{$site}_FILE_PUBLIC_PATH") ?: "$data/public";
+
 $settings = array(
   'config_sync_directory' => getenv("DRUPAL_{$site}_CONFIG_SYNC_DIRECTORY") ?: "$data/sync",
   'config_directory_name' => getenv("DRUPAL_{$site}_CONFIG_DIRECTORY_NAME") ?: "$data/config",
   'file_private_path' =>     getenv("DRUPAL_{$site}_FILE_PRIVATE_PATH")     ?: "$data/private",
   'hash_salt' =>             getenv("DRUPAL_{$site}_HASH_SALT_PATH")        ?: "$data/salt.key",
+  'file_public_path' =>      "sites/$site/files",
 );
-# We're putting the salt in its own file, so that it is persistent, portable, can be automatically generated, and will not be written to this file by drush.
-# Generate it if the file does not exist or is empty.
-if (!file_exists($settings['hash_salt']) || filesize($settings['hash_salt']) == 0) {
-  file_put_contents($settings['hash_salt'], Crypt::randomBytesBase64(55));
+
+# Create the directories, public files symlink, and hash_salt, as needed.
+
+use Drupal\Component\Utility\Crypt;
+
+if (!is_dir($data)) {
+  mkdir($data, recursive: true);
+}
+if (!is_dir($settings['file_private_path'])) {
+  mkdir($settings['file_private_path']);
+}
+if (!is_dir($settings['config_sync_directory'])) {
+  mkdir($settings['config_sync_directory']);
+}
+if (!is_dir($settings['config_directory_name'])) {
+  mkdir($settings['config_directory_name']);
+}
+if (!is_dir($file_public_path_real)) {
+  mkdir($file_public_path_real);
 }
 
-# Create the data directories as needed
-@mkdir($settings['file_private_path']);
-@mkdir($settings['config_sync_directory']);
-@mkdir($settings['config_directory_name']);
+if (!is_link($settings['file_public_path'])) {
+  symlink($file_public_path_real, $settings['file_public_path']);
+}
 
-# Location to keep the actual public files on
-$file_public_path_real = getenv("DRUPAL_{$site}_FILE_PUBLIC_PATH") ?: "$data/public";
-@mkdir($file_public_path_real);
-# If we change these away from the webroot, Drupal will not serve them
-# So we'll symlink our real dir to this one.
-$settings['file_public_path'] = "sites/$site/files";
-@symlink($file_public_path_real, $settings['file_public_path']);
-
-
-# All settings are overwritable by local.settings.php at the site's data directory
-# Using environment variables is preferred, though.
-if (file_exists("$data/local.settings.php")) {
-  include "$data/local.settings.php";
+if (!file_exists($settings['hash_salt'])) {
+  file_put_contents($settings['hash_salt'], Crypt::randomBytesBase64(55));
 }
